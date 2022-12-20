c9939f5256a139bfc59302d13a6748be6fbd9825
David Montaño <77401753+ndavidmb@users.noreply.github.com>
2022-12-20 09:25:17 -0500

    Add user preference

--- main.py
+++ main.py
@@ -21,107 +21,107 @@ class KeywordQueryEventListener(EventListener):
         array = [
             [
                 "Dashboard",
-                OpenUrlAction("https://console.cloud.google.com/home/dashboard"),
+                OpenUrlAction("https://console.cloud.google.com/home/dashboard?authuser=1"),
                 "images/gcp_logo.png"
             ],
             [
                 "Compute Engine",
-                OpenUrlAction("https://console.cloud.google.com/compute"),
+                OpenUrlAction("https://console.cloud.google.com/compute?authuser=1"),
                 "images/compute_icon.png"
             ],
             [
                 "Kubernetes Engine",
-                OpenUrlAction("https://console.cloud.google.com/kubernetes"),
+                OpenUrlAction("https://console.cloud.google.com/kubernetes?authuser=1"),
                 "images/kube_icon.png"
             ],
             [
                 "Cloud SQL",
-                OpenUrlAction("https://console.cloud.google.com/sql"),
+                OpenUrlAction("https://console.cloud.google.com/sql?authuser=1"),
                 "images/sql_icon.png"
             ],
             [
                 "App Engine",
-                OpenUrlAction("https://console.cloud.google.com/appengine"),
+                OpenUrlAction("https://console.cloud.google.com/appengine?authuser=1"),
                 "images/app_engine_icon.png"
             ],
             [
                 "Cloud Functions",
-                OpenUrlAction("https://console.cloud.google.com/functions"),
+                OpenUrlAction("https://console.cloud.google.com/functions?authuser=1"),
                 "images/cloud_functions_icon.png"
             ],
             [
                 "Datastore",
-                OpenUrlAction("https://console.cloud.google.com/datastore"),
+                OpenUrlAction("https://console.cloud.google.com/datastore?authuser=1"),
                 "images/datastore.png"
             ],
             [
                 "BigQuery",
-                OpenUrlAction("https://console.cloud.google.com/bigquery"),
+                OpenUrlAction("https://console.cloud.google.com/bigquery?authuser=1"),
                 "images/big_query.png"
             ],
             [
                 "Storage",
-                OpenUrlAction("https://console.cloud.google.com/storage"),
+                OpenUrlAction("https://console.cloud.google.com/storage?authuser=1"),
                 "images/storage_icon.png"
             ],
             [
                 "VPC Network",
-                OpenUrlAction("https://console.cloud.google.com/networking"),
+                OpenUrlAction("https://console.cloud.google.com/networking?authuser=1"),
                 "images/vpc_network_icon.png"
             ],
             [
                 "Network Services",
-                OpenUrlAction("https://console.cloud.google.com/net-services"),
+                OpenUrlAction("https://console.cloud.google.com/net-services?authuser=1"),
                 "images/network_services_icon.png"
             ],
             [
                 "Cloud Build",
-                OpenUrlAction("https://console.cloud.google.com/cloud-build"),
+                OpenUrlAction("https://console.cloud.google.com/cloud-build?authuser=1"),
                 "images/build_icon.png"
             ],
             [
                 "Container Registry",
-                OpenUrlAction("https://console.cloud.google.com/gcr"),
+                OpenUrlAction("https://console.cloud.google.com/gcr?authuser=1"),
                 "images/container_registry_icon.png"
             ],
             [
                 "Security",
-                OpenUrlAction("https://console.cloud.google.com/security"),
+                OpenUrlAction("https://console.cloud.google.com/security?authuser=1"),
                 "images/security_icon.png"
             ],
             [
                 "IAM Admin",
-                OpenUrlAction("https://console.cloud.google.com/iam-admin"),
+                OpenUrlAction("https://console.cloud.google.com/iam-admin?authuser=1"),
                 "images/iam_admin_icon.png"
             ],
             [
                 "Filestore",
-                OpenUrlAction("https://console.cloud.google.com/filestore"),
+                OpenUrlAction("https://console.cloud.google.com/filestore?authuser=1"),
                 "images/filestore_icon.png"
             ],
             [
                 "Stackdriver Logging",
-                OpenUrlAction("https://console.cloud.google.com/logs"),
+                OpenUrlAction("https://console.cloud.google.com/logs?authuser=1"),
                 "images/logging_icon.png"
             ],
             [
                 "Stackdriver Monitoring",
-                OpenUrlAction("https://console.cloud.google.com/monitoring"),
+                OpenUrlAction("https://console.cloud.google.com/monitoring?authuser=1"),
                 "images/monitoring_icon.png"
             ],
             [
                 "Billing",
-                OpenUrlAction("https://console.cloud.google.com/billing"),
+                OpenUrlAction("https://console.cloud.google.com/billing?authuser=1"),
                 "images/billing_icon.png"
             ],
             [
                 "Marketplace",
-                OpenUrlAction("https://console.cloud.google.com/marketplace"),
+                OpenUrlAction("https://console.cloud.google.com/marketplace?authuser=1"),
                 "images/marketplace_icon.png"
             ],
             [
                 "Pricing Calculator",
-                OpenUrlAction("https://cloud.google.com/products/calculator"),
+                OpenUrlAction("https://cloud.google.com/products/calculator?authuser=1"),
                 "images/pricing_calculator_icon.png"
             ]
         ]
